2cdea0d1a80a4b258c99f71ef91068cb243c4cff
maitc <maibeet.torres@gmail.com>
2017-05-16 13:45:46 -0400

    js funcionando mas carpeta assets

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//funcion ejercicio 1
+function principal(){
+	var boton = document.getElementById("demo");//se llama el id
+	boton.addEventListener("click", function(){//se realiza 'onclick' que se hacía en html.
+		alert("HOLA MUNDO");
+	});
+}
+principal();
+//ejercicios con on en html.
+function verificacionBody(){
+	alert("YA SE CARGÓ");
+}
+function verificacionInputUno(){
+	alert("Estás sobre mi 1313");
+}
+function verificacionInputDos(){
+	alert("No estás sobre mi");
+}
